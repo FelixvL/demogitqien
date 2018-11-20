adb80c5cfb1c010714a11f34be16e9623c29703b
FelixvL <digitaleservice@gmail.com>
2018-11-20 10:12:38 +0100

    nieuwe commit

--- Demo.java
+++ Demo.java
@@ -3,20 +3,29 @@ class Demo{
 	    public static void main(String[] args) {
 	    	System.out.println("Welkom");
 	    	ToegangsPoort tp = new ToegangsPoort();
-	    	tp.toelaten();
-	    	
+	    	Persoon persoon = new Persoon();
+	    	persoon.naam = "Fred";
+	    	int a = 24;
+	    	tp.toelaten(persoon);   // argument
+	    	cijferNoemen(23);
+	    }
+	    static void cijferNoemen(int getal) {
+	    	System.out.println(getal);
 	    }
 }
 class ToegangsPoort{
-	void toelaten() {
-		if (false) {
-		System.out.println("je bent toe gelaten?");
+	void toelaten(Persoon dePersoon) {  // parameter
+		if (true) {
+			System.out.println("je bent toe gelaten, welkom "+dePersoon.naam);
 		}else {
 			System.out.println("je bent niet toegelaten");
 		}
 	}
 }
-
+class Persoon{
+	String naam;
+//	Pasje pasje;
+}
 
 
 /*
