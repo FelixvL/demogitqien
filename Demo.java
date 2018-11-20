3bb6cff511b6272e55cafdb0b5163624a760bd5c
FelixvL <digitaleservice@gmail.com>
2018-11-20 09:39:51 +0100

    hhhhij doet het toegangspoort toegevoegd

--- Demo.java
+++ Demo.java
@@ -1,11 +1,18 @@
 
-import java.io.IOException;
-
 class Demo{
 	    public static void main(String[] args) {
 	    	System.out.println("Welkom");
+	    	ToegangsPoort tp = new ToegangsPoort();
+	    	tp.toelaten();
+	    	
 	    }
 }
+class ToegangsPoort{
+	void toelaten() {
+		System.out.println("je bent toe gelaten");
+	}
+}
+
 
 
 /*
